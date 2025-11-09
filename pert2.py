aa95a449463e08bd2fe507569617eb47537f5261
kylayusrina <kylayusrina@gmail.com>
2025-11-09 15:02:27 +0800

    Update pert2.py

---
+++ pert2.py
@@ -0,0 +1,27 @@
+# belajar variabel
+# cara kita menyimpan nilai
+
+# memberikan nilai (assignment)
+x = 10
+y = 6
+lebar = 13
+
+print(x + y + lebar)
+print("nilai dari x =", x)
+print("nilai dari y =", y)
+print("nilai dari lebar =", lebar)
+
+# tings to consider when making a variabel
+# hal2 yang hars diperhatikan ketila membuat variabel
+
+bet99 = 9 # tidak boleh diawali dengan angka
+jumlah_hutang = 1000000 # menggunakan dua suku kata harus digabungkan dengan _
+JumlahHutang = 1000000 # menggunakan dua suku kata harus digabungkan dengan huruf kapital
+
+# pemanggilan indirect
+z = lebar
+print("nilai dari z =", z)
+
+# Tugas
+# nama saya adalah : (kyla yusrina dimyati)
+# nim saya adalah : (25241121)
