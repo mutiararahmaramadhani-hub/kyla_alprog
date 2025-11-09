07570052b1a364f1ad61bc1a59a2ff085937c9b5
kylayusrina <kylayusrina@gmail.com>
2025-11-09 15:05:57 +0800

    Update pert3.py

---
+++ pert3.py
@@ -0,0 +1,18 @@
+# python
+# I will learn data type
+
+x = 10  # bilangan bulat (integer)
+print("nilai dari x =", x)
+print("tipe data dari x =", type(x))
+
+y = 12.5  # bilangan pecahan (float)
+print("nilai dari y =", y)
+print("tipe data dari y =", type(y))
+
+ukuran = "xxl"  # tipe data string
+print("nilai dari ukuran =", ukuran)
+print("tipe data dari ukuran =", type(ukuran))
+
+kebenaran = True  # tipe data boolean (True False)
+print("nilai dari kebenaran =", kebenaran)
+print("tipe data dari kebenaran =", type(kebenaran))
