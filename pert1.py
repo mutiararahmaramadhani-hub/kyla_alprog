24898d240cd973574be55b3268d81bda6de5b0b7
kylayusrina <kylayusrina@gmail.com>
2025-11-09 14:59:10 +0800

    Add files via upload

---
+++ pert1.py
@@ -0,0 +1,13 @@
+# saya sedang belajar python
+# code dibaca dari kiri kekanan
+# code dibaca dari atas ke bawah
+# cara untuk memindahkan satu baris, arahkan kursor pada baris yg ingin di pindahkan
+# tahan tombol alt, lalu tekan tombol arah bawah atau atas
+# cara untuk mengcopy satu baris full, sama dengan yg diatas
+# namun ditambahkan tahan tombol shif
+#print ("Nama saya bunga")
+#print("Saya adalah mahasiswa imout")
+#print("Tapi cerewet")
+x = 9
+y = 6
+print(x * y)
