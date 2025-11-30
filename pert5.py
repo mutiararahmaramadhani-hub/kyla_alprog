e53c072552ee09c3dce42589cbdc12b88bc69927
mutiararahmaramadhani-hub <mutiararahmaramadhani@gmail.com>
2025-11-30 17:14:13 +0800

    Create pert5.py

---
+++ pert5.py
@@ -0,0 +1,41 @@
+# operasi aritmatika : X : + -
+
+# operasi penjumlahan +
+a = 25
+b = 21 
+hasil = a + b
+print(a, "+", b, "=", hasil)
+
+# operasi pengurangan -
+hasil = a - b
+print(a, "-", b, "=", hasil)
+
+# operasi perkalian *
+hasil = a * b
+print(a, "*", b, "=", hasil)
+
+# operasi pembagian /
+hasil = a / b
+print(a, "/", b, "=", hasil)
+
+# operasi pangkat **
+hasil = a ** b
+print(a, "**", b, "=", hasil)
+
+# operasi modulus % (sisa bagi)
+hasil = a % b
+print(a, "mod", b, "=", hasil)
+
+# operasi floor division //
+hasil = a // b
+print(a, "floor", b, "=", hasil)
+
+# operasi aritmatika yang menggabungkan semua operasi di atas
+gabung = 10 - 6 / 4 % 6 // 5 + 2 * 3 ** 5
+print(gabung)
+
+# operasi prioritas:
+# 1. ()
+# 2. pangkat akar
+# 3. : X
+# 4. + -
